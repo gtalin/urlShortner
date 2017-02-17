134774a85a10007be89b5f844e1e198f08ff12e4
gtalin <gtalin@users.noreply.github.com>
2017-02-17 23:04:35 +0000

    connect to mLab db from local run node

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
-const Url = require('url').Url;
-//console.log(
-console.log(Url);
+var port = process.env.PORT || 3000;
+const validUrl = require('valid-url');
+//console.log(validUrl);
+//console.log(process.env);
 
-//DB stuff
+////DB stuff
 var mongodb = require("mongodb");
-var mongoClient = mongodb.mongoClient;
-var mongdbUrl = process.env.MONGOLAB_URI;//place where mongodb running
-//we'll connect when there is some request
+var MongoClient = mongodb.MongoClient;
+////console.log(MongoClient);
+console.log(process.env);
+var mongodbUrl = process.env.MONGOLAB_URI;//place where mongodb running
+console.log(mongodbUrl);
+
+////we'll connect when there is some request
 
 
 app.get("/", function(req,res) {
@@ -19,17 +23,31 @@ app.get("/", function(req,res) {
 app.get("/new/*", function(req,res) {
   //if we do this: "/new/:url" it'll not work
   console.log("this page is for urls");
+  var shortUrl = req.protocol + '://' + req.get('host');
+  console.log(shortUrl);
+  var result={"error":
+  "Wrong url format, make sure you have a valid protocol and real site."}
   var url = req.params[0];
   console.log(url);
-  try {
-    const myURL = new Url(url);
-    console.log("url passed", myURL.href);
-    res.send(myURL);
+  if (validUrl.isUri(url)) {
+    console.log("valid url");
+    MongoClient.connect(mongodbUrl, function(err,db){
+      if (err) {console.log("Unable to connect to db server. Error: ", err);}
+      else {
+        console.log("Connected");
+        //We'll add url
+        console.log(db.collection.count());
+        var collection = db.collection("urlshortner");
+        //it doesn't have anything yet.
+        //we'll add url and shortened url
+        db.close();
+      }
+    })
+    result={"original_url":url,
+    "short_url":"To-Do"}
   }
-  catch (err) {
-    console.log("Not valid url", err);
-    res.send("Error in url")
-  }
+  res.send(result);
+
 });
 
 app.get("/new/:url", function(req,res) {
@@ -50,7 +68,11 @@ app.get("/new/:url", function(req,res) {
 
 app.get("/:short", function(req,res) {
   console.log(req.params.short);
-  //will connect to mLab DB and
+  var errorMsg = {
+    "error":"This url is not on the database."};
+  //will connect to mLab DB
+  //Query db. If exist in db, redirect to
+  //retrieved url else error msg.
 });
 
 
